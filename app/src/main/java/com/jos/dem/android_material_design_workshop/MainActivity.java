20a2ad3e7704a1a0d5f9f5b216a8091a02877f15
josdem <joseluis.delacruz@gmail.com>
2017-07-27 22:25:50 -0500

    Setting status bar colors

--- app/src/main/java/com/jos/dem/android_material_design_workshop/MainActivity.java
+++ app/src/main/java/com/jos/dem/android_material_design_workshop/MainActivity.java
@@ -1,9 +1,10 @@
 package com.jos.dem.android_material_design_workshop;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
