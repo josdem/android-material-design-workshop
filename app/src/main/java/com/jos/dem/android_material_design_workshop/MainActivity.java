c5d1367268438b8e2da0f048eb05ed2187569ea2
josdem <joseluis.delacruz@gmail.com>
2017-07-27 21:34:17 -0500

    Starting Material Design Workshop :)

---
+++ app/src/main/java/com/jos/dem/android_material_design_workshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jos.dem.android_material_design_workshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
